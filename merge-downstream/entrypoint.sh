d87b51b625529bb61919ff163cbaf081bcd22f23
Miles Crabill <miles@milescrabill.com>
2019-10-20 14:45:44 -0700

    add branch specifier, move host check ignore up

--- merge-downstream/entrypoint.sh
+++ merge-downstream/entrypoint.sh
@@ -18,6 +18,10 @@ git config --global push.default matching
 mkdir -p ~/.ssh
 ssh-agent -a ${SSH_AUTH_SOCK} > /dev/null
 
+# FIXME
+# adding the host key doesn't work ?!, this is an unfortunate override
+git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+
 # add github's pubkey to known_hosts
 echo 'github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==' >> ~/.ssh/known_hosts
 
@@ -46,10 +50,6 @@ fi
 # actions/checkout step has to be run beforehand
 # this dir is mounted into the container
 cd $GITHUB_WORKSPACE
-
-# FIXME remove
-echo $SSH_AUTH_SOCK
-git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 
 # get branches for all remotes
 git fetch --all
